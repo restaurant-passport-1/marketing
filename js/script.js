3e32202deb1e278843ee61b8a2e2c315bfa74861
Mike Sinnema <msinnema33@gmail.com>
2020-01-08 20:20:30 -0700

    started on hamburger, need to get menu items to show

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*mobile nav */
+
+// $('.js--nav-icon').click(function() {
+//     var nav = $('.js--main-nav');
+//     var icon = $('.js--nav-icon i');
+
+//     nav.slideToggle(200);
+
+//     if (icon.hasClass('fas fa-bars')) {
+//         icon.addClass('fas fa-times');
+//         icon.removeClass('fas fa-bars');
+//     } else {
+//         icon.addClass('fas fa-bars');
+//         icon.removeClass('fas fa-times');
+//     }
+// });
